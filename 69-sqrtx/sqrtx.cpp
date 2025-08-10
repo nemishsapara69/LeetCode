bd0001d075db7ca242a047b3a5d7475e80db435e
Nemish Spara <164607451+nemishsapara69@users.noreply.github.com>
2025-08-10 21:42:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (85.02%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        
         if (x == 0 || x == 1)
             return x;
-        
-        
+
         int start = 1;
         int end = x;
         int mid = -1;
